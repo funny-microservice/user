27ef0900b6e3fafdc3b7c04cdef24a95f1930210
xiekun <xiekun@f3dt.com>
2020-09-17 16:20:33 +0800

    initial commit

--- libs/db.js
+++ libs/db.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize')
 const User = require('../models/user')
 const sequelize = new Sequelize('mysql://dev:abc,.123@192.168.3.4:13306/demo_user_db', {
   pool: {
-    max: 30,
+    max: 18, // 8 nodes
+    // max: 37, 4 nodes
     min: 5,
     idle: 10000
   },
